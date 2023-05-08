b70e0e85067da91a0e9ec270788de0a19a8fba79
Ukesh Rajasekar <z5288900@ad.unsw.edu.au>
2023-05-08 19:35:47 +1000

    feat: todo collection update

--- models/todoModel.js
+++ models/todoModel.js
@@ -14,6 +14,10 @@ const todoSchema = new mongoose.Schema({
         type: String,
         trim: true
     },
+    isCompleted: {
+        type: Boolean,
+        default: false
+    },
     createdAt: {
         type: Date,
         default: Date.now()
--- routers/todoRouter.js
+++ routers/todoRouter.js
@@ -4,7 +4,7 @@ const todoController = require('../controllers/todoController');
 const router = Router();
 
 
-router.route(':id').get(todoController.getTodo).put(todoController.updateTodo).delete(todoController.deleteTodo);
+router.route('/:id').get(todoController.getTodo).put(todoController.updateTodo).delete(todoController.deleteTodo);
 router.route(`/`).post(todoController.createTodo).get(todoController.getAllTodos);
 router.route(`/todos/removeAll`).delete(todoController.deleteAll);
 
